bdd29813da2f5039990284c066ddb80dd3736e88
Sreeraj <154326486+C-O-D-E-G-A-M-E-R@users.noreply.github.com>
2024-08-10 13:35:22 +0530

    Add files via upload

---
+++ studentmarklistdatamanagementsystem/code.rb
@@ -0,0 +1,86 @@
+def studentinfo()
+mark=nil
+grade=nil
+class1=nil
+name=nil
+condition1=nil
+writingcondition=nil
+limit=nil
+path=nil
+puts"\n"
+puts "Enter the Operation\n Write for write  and Read for read "
+puts"\n"
+condition=gets.chomp();
+puts"\n"
+if condition=="write"||condition=="Write"||condition=="WRITE"
+
+  def writing()
+    puts"Enter The Path For Save File\n example Drivename://foldername/filname.type"
+    puts"\n"
+    path=gets.chomp();
+    puts"\n"
+  puts"Enter How many students"
+  puts"\n"
+  limit=gets.chomp();
+  puts"\n"
+  puts"Enter the writing condition w for write "
+  puts"\n"
+  writingcondition=gets.chomp()
+  puts"\n"
+  i=0;
+  while i < limit.to_i
+puts "Enter The Student Name";
+puts"\n"
+name=gets.chomp()
+puts"\n"
+puts "Enter The student mark";
+puts"\n"
+mark=gets.chomp()
+puts"\n"
+puts "Enter The student class";
+puts"\n"
+class1=gets.chomp();
+puts"\n"
+puts "Enter The student grade";
+puts"\n"
+grade=gets.chomp()
+puts"\n"
+File.open(path,writingcondition)do|file|
+
+   file.write("Name:"+name+"\t\t"+"Mark:"+mark+"\t\t"+"Class:"+class1+"\t\t"+"Grade:"+grade+"\n")
+   i+=1;
+  end
+  puts "Enter the writing condition to a for append"
+  puts"\n"
+  writingcondition=gets.chomp()
+    puts"\n"
+end
+end
+writing()
+
+
+elsif condition=="read"||condition=="Read"||condition=="READ"
+
+  def reading()
+puts "Enter The File Path example\nDrivename://foldername/filename.type"
+puts"\n"
+path=gets.chomp();
+puts"\n"
+File.open(path,"r")do|file|
+
+for lines in file.readlines()
+  puts lines
+end
+end
+end
+reading()
+
+else
+  puts "invalid"
+
+end
+
+
+end
+  
+studentinfo()
